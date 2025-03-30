ad7a36c4d0bb5d5ee77b5e00ff8d1df1f485c8d8
Natanael Melo <109748470+Natanael135@users.noreply.github.com>
2025-03-30 09:29:48 -0300

    chore: otimizando estrutura inicial

--- src/routes/paths.jsx
+++ src/routes/paths.jsx
@@ -1,20 +1,13 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import LayoutPage from "../layout/LayoutPage";
+import { Routes, Route } from "react-router-dom";
+import LayoutPage from "../layout";
 import HomePage from "../Pages/Home";
 
-
-const Paths = () => {
-  return (
-    <>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<LayoutPage />}>
-            <Route index element={<HomePage />} />
-          </Route>
-        </Routes>
-      </BrowserRouter>
-    </>
-  );
-};
+const Paths = () => (
+  <Routes>
+    <Route path="/" element={<LayoutPage />}>
+      <Route index element={<HomePage />} />
+    </Route>
+  </Routes>
+);
 
 export default Paths;
