f5c10044f3a4ea9b151fbab0a929b5bc1d09dab2
Natanael Melo <109748470+Natanael135@users.noreply.github.com>
2025-03-30 10:19:21 -0300

    chore: atualizando estrutura inicial do projeto.

--- src/layout/index.jsx
+++ src/layout/index.jsx
@@ -1,31 +1,28 @@
-import { Outlet } from 'react-router-dom';
-import Header from '../components/Header';
-import Footer from '../components/Footer';
-import styled from 'styled-components';
+import { Outlet } from "react-router-dom";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+import styled from "styled-components";
 
 const Layout = styled.div`
-  @media (min-width: 320px) {
-    margin: 0 1rem 0 1rem;
-  }
-  @media (min-width: 1024px) {
-    margin: 0 4rem 0 4rem;
-  }
-  @media (min-width: 1920px) {
-    margin: 0 8rem 0 8rem;
-  }
-  @media (min-width: 2560px) {
-    margin: 0 20rem 0 20rem;
-  }
+  display: flex;
+  flex-direction: column;
+  min-height: 100dvh;
 `;
+
+const MainContent = styled.main`
+  flex: 1;
+  padding: 20px;
+`;
+
 const LayoutPage = () => {
   return (
-    <>
-      <Layout>
-        <Header />
+    <Layout>
+      <Header />
+      <MainContent>
         <Outlet />
-        <Footer />
-      </Layout>
-    </>
+      </MainContent>
+      <Footer />
+    </Layout>
   );
 };
 
